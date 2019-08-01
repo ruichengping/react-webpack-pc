5145d2ccdb29e1daf4e14332675236a674e8c628
wanghao <wanghao1@codemao.cn>
2019-08-01 17:40:07 +0800

    格式化

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,32 +1,28 @@
-'use strict'
-const path = require('path')
-const utils = require('./utils')
-const config = require('../config')
-const tsImportPluginFactory = require('ts-import-plugin')
+"use strict";
+const path = require("path");
+const utils = require("./utils");
+const config = require("../config");
+const tsImportPluginFactory = require("ts-import-plugin");
 
-function resolve (dir) {
-  return path.join(__dirname, '..', dir)
+function resolve(dir) {
+  return path.join(__dirname, "..", dir);
 }
 
-
-
 module.exports = {
-  context: path.resolve(__dirname, '../'),
+  context: path.resolve(__dirname, "../"),
   entry: {
-    app: './src/main.tsx'
+    app: "./src/main.tsx"
   },
   output: {
     path: config.build.assetsRoot,
-    filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+    filename: "[name].js",
+    publicPath: process.env.NODE_ENV === "production" ? config.build.assetsPublicPath : config.dev.assetsPublicPath
   },
   resolve: {
-    alias:{
-      '@':path.join(__dirname,'../src')
+    alias: {
+      "@": path.join(__dirname, "../src")
     },
-    extensions: ['.ts','.tsx','.js', '.jsx', '.json']
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
   },
   // externals: {
   //   'lodash':'_'
@@ -35,15 +31,15 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'ts-loader',
+        loader: "ts-loader",
         options: {
           getCustomTransformers: () => ({
-            transpileOnly:true,
+            transpileOnly: true,
             before: [
               tsImportPluginFactory({
-                  libraryDirectory: 'es',
-                  libraryName: 'antd',
-                  style: true
+                libraryDirectory: "es",
+                libraryName: "antd",
+                style: true
               })
             ]
           })
@@ -51,26 +47,26 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+          name: utils.assetsPath("img/[name].[hash:7].[ext]")
         }
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+          name: utils.assetsPath("media/[name].[hash:7].[ext]")
         }
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+          name: utils.assetsPath("fonts/[name].[hash:7].[ext]")
         }
       }
     ]
@@ -81,10 +77,10 @@ module.exports = {
     setImmediate: false,
     // prevent webpack from injecting mocks to Node native modules
     // that does not make sense for the client
-    dgram: 'empty',
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-    child_process: 'empty'
+    dgram: "empty",
+    fs: "empty",
+    net: "empty",
+    tls: "empty",
+    child_process: "empty"
   }
-}
+};
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,19 +1,16 @@
-'use strict'
-const path = require('path')
-const utils = require('./utils')
-const webpack = require('webpack')
-const config = require('../config')
-const merge = require('webpack-merge')
-const baseWebpackConfig = require('./webpack.base.conf')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-
-
-
+"use strict";
+const path = require("path");
+const utils = require("./utils");
+const webpack = require("webpack");
+const config = require("../config");
+const merge = require("webpack-merge");
+const baseWebpackConfig = require("./webpack.base.conf");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 const webpackConfig = merge(baseWebpackConfig, {
   module: {
@@ -22,33 +19,31 @@ const webpackConfig = merge(baseWebpackConfig, {
       usePostCSS: true
     })
   },
-  mode: 'production',
+  mode: "production",
   devtool: config.build.productionSourceMap ? config.build.devtool : false,
   output: {
     path: config.build.assetsRoot,
-    filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    filename: utils.assetsPath("js/[name].[chunkhash].js"),
+    chunkFilename: utils.assetsPath("js/[id].[chunkhash].js")
   },
-  optimization:{
-    splitChunks:{
-      chunks: 'all',
+  optimization: {
+    splitChunks: {
+      chunks: "all",
       cacheGroups: {
         vendors: {
-          test: /[\\/]node_modules[\\/](react|react-dom|lodash)[\\/]/,
+          test: /[\\/]node_modules[\\/](react|react-dom|lodash)[\\/]/
         },
-        antdesign:{
+        antdesign: {
           test: /[\\/]node_modules[\\/]@ant-design[\\/]/
         }
       }
     },
     runtimeChunk: {
-      name: 'runtime'
+      name: "runtime"
     },
-    minimizer:[
+    minimizer: [
       new OptimizeCSSAssetsPlugin({
-        cssProcessorOptions: config.build.productionSourceMap
-          ? { safe: true, map: { inline: false } }
-          : { safe: true }
+        cssProcessorOptions: config.build.productionSourceMap ? { safe: true, map: { inline: false } } : { safe: true }
       }),
       new UglifyJsPlugin({
         uglifyOptions: {
@@ -56,25 +51,25 @@ const webpackConfig = merge(baseWebpackConfig, {
             warnings: false
           }
         },
-        cache:true,
+        cache: true,
         parallel: true,
-        sourceMap: config.build.productionSourceMap 
-      }),
+        sourceMap: config.build.productionSourceMap
+      })
     ]
   },
   plugins: [
     new CleanWebpackPlugin(),
     // extract css into its own file
     new MiniCssExtractPlugin({
-      filename: utils.assetsPath('css/[name].[contenthash].css'),
-      chunkFilename: utils.assetsPath('css/[id].css')
+      filename: utils.assetsPath("css/[name].[contenthash].css"),
+      chunkFilename: utils.assetsPath("css/[id].css")
     }),
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
     // see https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: config.build.index,
-      template: 'index.html',
+      template: "index.html",
       inject: true,
       minify: {
         removeComments: true,
@@ -84,7 +79,7 @@ const webpackConfig = merge(baseWebpackConfig, {
         // https://github.com/kangax/html-minifier#options-quick-reference
       },
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-      chunksSortMode: 'dependency'
+      chunksSortMode: "dependency"
     }),
     // keep module.id stable when vendor modules does not change
     new webpack.HashedModuleIdsPlugin(),
@@ -92,35 +87,31 @@ const webpackConfig = merge(baseWebpackConfig, {
     // copy custom static assets
     new CopyWebpackPlugin([
       {
-        from: path.resolve(__dirname, '../static'),
+        from: path.resolve(__dirname, "../static"),
         to: config.build.assetsSubDirectory,
-        ignore: ['.*']
+        ignore: [".*"]
       }
     ])
   ]
-})
+});
 
 if (config.build.productionGzip) {
-  const CompressionWebpackPlugin = require('compression-webpack-plugin')
+  const CompressionWebpackPlugin = require("compression-webpack-plugin");
 
   webpackConfig.plugins.push(
     new CompressionWebpackPlugin({
-      asset: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: new RegExp(
-        '\\.(' +
-        config.build.productionGzipExtensions.join('|') +
-        ')$'
-      ),
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: new RegExp("\\.(" + config.build.productionGzipExtensions.join("|") + ")$"),
       threshold: 10240,
       minRatio: 0.8
     })
-  )
+  );
 }
 
 if (config.build.bundleAnalyzerReport) {
-  const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-  webpackConfig.plugins.push(new BundleAnalyzerPlugin())
+  const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+  webpackConfig.plugins.push(new BundleAnalyzerPlugin());
 }
 
-module.exports = webpackConfig
+module.exports = webpackConfig;
